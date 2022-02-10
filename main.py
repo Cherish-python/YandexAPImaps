722f06d5ca1a471a9494e389eb847ab9c6b97ace
MrTarariev <95866482+MrTarariev@users.noreply.github.com>
2022-02-10 18:37:10 +0300

    Рабочий ответ к заданию №1

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
 import sys
 
+import requests
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
 
 class MainScreen(QMainWindow):
     def __init__(self):
@@ -8,8 +10,14 @@ class MainScreen(QMainWindow):
         self.setGeometry(500, 100, 700, 700)
         self.setWindowTitle('работа с картой')
         self.map_frame = QLabel(self)
-        # self.map_frame.setPixmap()  сюда добавишь картинку
-        self.map_frame.resize(self.map_frame.pixmap().size())
+        response = requests.get("http://static-maps.yandex.ru/1.x/?ll=37.530887,55.703118&spn=0.002,0.002&l=map")
+        self.map_file = "map.png"
+        with open(self.map_file, "wb") as file:
+            file.write(response.content)
+        self.pixmap = QPixmap(self.map_file)
+        self.map_frame.setPixmap(self.pixmap)
+        self.map_frame.move(100, 100)
+        self.map_frame.resize(500, 500)
 
 
 if __name__ == '__main__':
